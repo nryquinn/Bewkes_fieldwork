811d7d8e09048780c698c4948d0ba312359ee766
Nick Quinn <Nick@nicks-air.colgate.edu>
2018-06-13 15:38:06 -0400

    downloaded data from server

--- SapFlow.R
+++ SapFlow.R
@@ -1,1 +1,4 @@
 #Bewkes Project
+os.flag <- 2
+source("/Users/Nick/Documents/GitHub/bewkes_field/sapflux_process.r")
+metA <- read.csv("/Volumes/data/data_repo/field_data/bewkes/sensor/decagon/met_air.csv")
